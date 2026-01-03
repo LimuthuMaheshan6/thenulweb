ad3c290d5e7443b0749ba00a62b60d9260f09444
Yasith Manthrirathna <limuthumaheshan2@gmail.com>
2026-01-03 09:43:37 +0530

    Project initialized 2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import './+layout.scss';
+	import './xtras.css';
+    import { onMount } from 'svelte';
+	import Navbar from '$lib/Navbar.svelte';
+
+	onMount(async () => {
+		let font = new FontFace("inter", "url(../src/inter.woff2)")
+
+		await font.load()
+
+		document.fonts.add(font)
+		
+	})
+
+
+	
+</script>
+<Navbar></Navbar>
+<slot></slot>
+
+
+
+
