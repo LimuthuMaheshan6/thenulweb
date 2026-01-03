4522d9656548727a70a43fdd76176bc5c1515a4a
Yasith Manthrirathna <limuthumaheshan2@gmail.com>
2026-01-03 15:32:11 +0530

    Header Made

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<header style="background: rgb(228, 220, 201);">
+    <br><br><br>
+    <h1 class="text-center text-[#441f1f]">Ceylon Made Cinnamon</h1>
+    <p class="text-[grey] text-center text-[21px]">"Helping societies by exporting cinnamon world wide."</p>
+    <div>
+        
+            <div class="mx-auto overflow-hidden min-[1024px]:h-[700px] min-[1024px]:rounded-2xl min-[1024px]:w-[1050.41px] " >
+                <img class="mx-auto min-[1024px]:rounded-2xl min-[1024px]:h-[700px] hover:scale-110 duration-300" src="cinnamon.webp" alt="cinnamon">
+            </div>
+    </div>
+
+
+
+
+
+
+
+</header>
+
+<h1>hello</h1>
